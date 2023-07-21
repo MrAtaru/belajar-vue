686a01c6b1340fde142288d0574cc2813943d808
MrAtaru <zrx655@gmail.com>
2023-07-21 14:20:28 +0700

    belajar vue

---
+++ src/MyComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h2>Halo, {{ nama }}</h2>
+        <p>Umur kamu: {{ umur }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['nama', 'umur'], //definisi props "nama" dan "umur"
+}
+</script>
